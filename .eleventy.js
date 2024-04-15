f734756ea3472212092c503cb710e6d538578cd2
Zlushiie <zlushiiie@gmail.com>
2024-04-14 20:49:56 -0400

    removed unused assets, added CV and changed zWeather description.

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,5 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/assets/img/*");
-  eleventyConfig.addPassthroughCopy("src/assets/notes/*");
+  eleventyConfig.addPassthroughCopy("src/assets/*");
   eleventyConfig.ignores.add("src/links/index.liquid");
 
   return {
